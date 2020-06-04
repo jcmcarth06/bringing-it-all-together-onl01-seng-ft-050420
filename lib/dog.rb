bf063ce0b47c79b86c00e9185a85077e572a5599
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-04 23:30:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,6 +56,16 @@ class Dog
     Dog.new(id: result[0], name: result[1], breed: result[2])
   end
 
-  
+  def self.find_or_create_by(name:, breed:)
+    song = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Song.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      dog = self.create(name: name, breed: album)
+    end
+    dog
+  end
+    
 
 end
