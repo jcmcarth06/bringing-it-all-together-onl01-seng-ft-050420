9853a10966bcf94f96600e58fba8379c121e7be1
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-04 23:03:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,12 +25,16 @@ class Dog
   end
 
   def save
+    if self.id
+      self.update
+    else
     sql = <<-SQL
       INSERT INTO dogs (name, breed)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute(SELECT )
   end
 
   def self.create(name, breed)
